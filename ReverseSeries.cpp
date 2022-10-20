509860b03b177bcdbc5b4c30d6dc4b37fe39195b
kshamaj15 <kshamaj150896@gmail.com>
2022-10-20 16:21:34 +0530

    reverseSeries

---
+++ ReverseSeries.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		long long int reverse_digit(long long int n)
+		{
+		    // Code here
+		    int long long num= n;
+		    int long long revNum = 0;
+		    while(num>0)
+		     { int digit = num%10;
+		      revNum = digit + revNum*10;
+	     	   num=num/10;}
+		    
+		    return revNum; 
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int n;
+    	cin >> n;
+    	Solution ob;
+    	long long int  ans = ob.reverse_digit(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
